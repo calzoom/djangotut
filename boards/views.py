e59cfbadc8cf864bd386cc10fbe4ad9122998c0d
Japjot <calzoom@berkeley.edu>
2021-07-26 16:41:08 -0700

    new forms

--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse, Http404
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 # Create your views here.
 # Views are Python functions that receive
@@ -24,19 +25,24 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
 
     if request.method == "POST":
-        subject = request.POST["subject"]
-        message = request.POST["message"]
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
 
-        user = User.objects.first()  # TODO: get the currently logged in user
+            post = Post.objects.create(
+                message=form.cleaned_data.get("message"), topic=topic, created_by=user
+            )
 
-        topic = Topic.objects.create(subject=subject, board=board, starter=user)
+            return redirect(
+                "board_topics", pk=board.pk
+            )  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
 
-        post = Post.objects.create(message=message, topic=topic, created_by=user)
-
-        return redirect(
-            "board_topics", pk=board.pk
-        )  # TODO: redirect to the created topic page
-
-    return render(request, "new_topic.html", {"board": board})
+    return render(request, "new_topic.html", {"board": board, "form": form})
